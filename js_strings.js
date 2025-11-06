5a973953ee0bb455d4422d4645c4545eb8be21c8
JustusKu <asembopunga@gmail.com>
2025-11-06 23:32:44 +0300

    word replacement

--- js_strings.js
+++ js_strings.js
@@ -2,7 +2,7 @@
 let str = prompt("Enter a string");
 //console.log(str);
 //check if string is UPPERCASE
-if (str === str.toUpperCase()){
+/*if (str === str.toUpperCase()){
     console.log("UPPERCASE");
 }
 //check string if is lowercase
@@ -11,14 +11,25 @@ else if (str === str.toLowerCase()){
 }
 //check if string is mixed
 else{
-    console.log("Mixed")
+    console.log("Mixed");
 }
 
 //checks if the string starts with letter 'A'
-//let startWith = str.startsWith("A");
 if (str.startsWith("A")){
-    console.log("Starts with A")
+    console.log("Starts with A");
 }
 else {
-    console.log("Does not start with A")
+    console.log("Does not start with A");
+}*/
+//replace 'sad' with 'happy'
+let str1 = "I am sad today";
+let str2 = str1.replace("sad", "happy");
+console.log(str2)
+
+//checks if string includes symbol @
+if (str.includes("@")){
+    console.log("Valid email");
+}
+else{
+    console.log("Invalid email");
 }
