dda621a2a7b82a811a6f8603d847395ec6423005
JustusKu <asembopunga@gmail.com>
2025-11-10 08:29:28 +0300

    complete code

--- js_strings.js
+++ js_strings.js
@@ -2,7 +2,7 @@
 let str = prompt("Enter a string");
 //console.log(str);
 //check if string is UPPERCASE
-/*if (str === str.toUpperCase()){
+if (str === str.toUpperCase()){
     console.log("UPPERCASE");
 }
 //check string if is lowercase
@@ -12,41 +12,45 @@ else if (str === str.toLowerCase()){
 //check if string is mixed
 else{
     console.log("Mixed");
-}*/
+}
 
-/*//2. checks if the string starts with letter 'A'
+//2. checks if the string starts with letter 'A'
 if (str.startsWith("A")){
     console.log("Starts with A");
 }
 else {
     console.log("Does not start with A");
-}*/
-   /*//3. replace 'sad' with 'happy'
+}
+
+//3. replace 'sad' with 'happy'
 let str1 = "I am sad today";
 let str2 = str1.replace("sad", "happy");
-console.log(str2)*/
+console.log(str2);
 
-/*//4. checks if string includes symbol @
+//4. checks if string includes symbol @
 if (str.includes("@", ".")){
     console.log("Valid email");
 }
 else{
     console.log("Invalid email");
-}*/
+}
+
 //5. checks if strings is a question
-/*if (str.endsWith("?")){
+if (str.endsWith("?")){
     console.log("This is a question");
 }
 else{
     console.log("Not a question");
-}*/
+}
+
 //6. getting last 3 digits
-/*let num = str.slice(-3);
+let num = str.slice(-3);
 //replacing rest of numbers
 let num2 = "*".repeat(str.length-3) + num;
-console.log(num2)*/
+console.log(num2);
+
 //7. middle character
-/*let middle = Math.floor(str.length/2);
+let middle = Math.floor(str.length/2);
 //return 2 middle characters
 if(str.length%2 === 0){
     console.log(str.slice(middle -1, middle +1));
@@ -54,8 +58,9 @@ if(str.length%2 === 0){
 //return 1 character
 else{
     console.log(str.charAt(middle));
-}*/
-/*//8. compare 2 strings
+}
+
+//8. compare 2 strings
 let str3 = prompt("Enter a string2");
 //convert to lowercase
 if (str.toLowerCase()=== str3.toLowerCase()){
@@ -63,21 +68,23 @@ if (str.toLowerCase()=== str3.toLowerCase()){
 }
 else{
     console.log("Not a match");
-}*/
+}
+
 //9. checking dot include
-/*if (str.includes("dumb")){
+if (str.includes("dumb")){
     let filteredtWord = str.replace("dumb", "***");
     console.log("Inappropriate word filtered");
     console.log(filteredtWord);
 }
 else{
     console.log("Clean sentence");
-}*/
-/*//10. getting last index space
+}
+
+//10. getting last index space
 let space = str.lastIndexOf(" ");
 //extract last word
 let lastWord = str.slice(space +1);
-console.log("Last word is:", lastWord);*/
+console.log("Last word is:", lastWord);
 
 
 
